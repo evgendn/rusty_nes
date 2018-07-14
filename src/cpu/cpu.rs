a1bcd6b9f27df1fe9370469a2edcdd0152f867be
evgendn <evgeniydein@gmail.com>
2018-07-15 01:22:24 +0500

    Update cpu

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -1,8 +1,7 @@
-use rom;
 use utils;
-use nes::NES;
 use cpu::opcode::*;
-use cpu::instruction::*;
+use cpu::instructions::*;
+use ram;
 
 #[derive(Debug)]
 pub struct CPU {
@@ -17,6 +16,7 @@ pub struct CPU {
     pub sp_reg: u8,
     // Program counter
     pub pc_reg: u16,
+    pub ram: ram::RAM,
 }
 
 // 7  bit  0
@@ -118,7 +118,7 @@ impl StatusRegister {
 
 impl CPU {
     // Power up
-    pub fn new() -> CPU {
+    pub fn new(ram: &mut ram::RAM) -> CPU {
         CPU {
             a_reg: 0x0,
             x_reg: 0x0,
@@ -126,6 +126,7 @@ impl CPU {
             p_reg: StatusRegister::new(),
             sp_reg: 0xfd,
             pc_reg: 0x0,
+            ram: *ram,
         }
     }
 
@@ -134,59 +135,63 @@ impl CPU {
         self.p_reg.set_interrupt_flag(true);
     }
 
-    pub fn tick(&mut self, nes: &mut NES) {
+    pub fn tick(&mut self) {
         let opcodes= &MAP;
         let address = self.pc_reg as u8;
         let opcode = opcodes.get(&address).unwrap();
         println!("{:?}", opcode);
-        let fetched_address = self.chose_addressing_mode(nes, opcode);
-        self.execute_instruction(nes, fetched_address, opcode);
+        let fetched_address = self.choose_addressing_mode(opcode) as u8;
+        self.execute_instruction(fetched_address, opcode);
     }
 
-    pub fn chose_addressing_mode(&mut self, nes: &mut NES, opcode: &OpCode) -> u16 {
+    pub fn choose_addressing_mode(&mut self, opcode: &OpCode) -> u16 {
         match opcode.mode {
             AddressingMode::Implicit => 0x0000,
-            AddressingMode::Accumulator => 0x0000,
-            AddressingMode::Immediate => self.fetch_immidiate(nes) as u16,
-            AddressingMode::ZeroPage => self.fetch_zero_page(nes),
-            AddressingMode::ZeroPageX => self.fetch_zero_page_x(nes),
-            AddressingMode::ZeroPageY => self.fetch_zero_page_y(nes),
-            AddressingMode::Absolute => self.fetch_absolute(nes),
-            AddressingMode::AbsoluteX => self.fetch_absolute_x(nes),
-            AddressingMode::AbsoluteY => self.fetch_absolute_y(nes),
-            AddressingMode::Relative => self.fetch_relative(nes) as u16,
-            AddressingMode::IndirectX => self.fetch_indirect_x(nes),
-            AddressingMode::IndirectY => self.fetch_indirect_y(nes),
+            AddressingMode::Accumulator => self.fetching_accumulator() as u16,
+            AddressingMode::Immediate => self.fetch_immidiate() as u16,
+            AddressingMode::ZeroPage => self.fetch_zero_page(),
+            AddressingMode::ZeroPageX => self.fetch_zero_page_x(),
+            AddressingMode::ZeroPageY => self.fetch_zero_page_y(),
+            AddressingMode::Absolute => self.fetch_absolute(),
+            AddressingMode::AbsoluteX => self.fetch_absolute_x(),
+            AddressingMode::AbsoluteY => self.fetch_absolute_y(),
+            AddressingMode::Relative => self.fetch_relative() as u16,
+            AddressingMode::IndirectX => self.fetch_indirect_x(),
+            AddressingMode::IndirectY => self.fetch_indirect_y(),
             _ => 0x0
         } 
     }
 
-    pub fn fetch_immidiate(&mut self, nes: &mut NES) -> u8 {
-        let address = nes.wram.read_byte(self.pc_reg);
+    pub fn fetching_accumulator(&mut self) -> u8 {
+        self.a_reg
+    }
+
+    pub fn fetch_immidiate(&mut self) -> u8 {
+        let address = self.ram.read_byte(self.pc_reg);
         self.pc_reg += 1;
         address
     }
 
-    pub fn fetch_zero_page(&mut self, nes: &mut NES) -> u16 {
-        let address = nes.wram.read_byte(self.pc_reg);
+    pub fn fetch_zero_page(&mut self) -> u16 {
+        let address = self.ram.read_byte(self.pc_reg);
         self.pc_reg += 1;
         address as u16
     }
 
-    pub fn fetch_zero_page_x(&mut self, nes: &mut NES) -> u16 {
-        let address = nes.wram.read_byte(self.pc_reg);
+    pub fn fetch_zero_page_x(&mut self) -> u16 {
+        let address = self.ram.read_byte(self.pc_reg);
         self.pc_reg += 1;
         (address as u16 + self.x_reg as u16) & 0xff as u16
     }
 
-    pub fn fetch_zero_page_y(&mut self, nes: &mut NES) -> u16 {
-        let address = nes.wram.read_byte(self.pc_reg);
+    pub fn fetch_zero_page_y(&mut self) -> u16 {
+        let address = self.ram.read_byte(self.pc_reg);
         self.pc_reg += 1;
         (address as u16 + self.y_reg as u16) & 0xff as u16
     }
 
-    pub fn fetch_relative(&mut self, nes: &mut NES) -> u8 {
-        let address = nes.wram.read_byte(self.pc_reg);
+    pub fn fetch_relative(&mut self) -> u8 {
+        let address = self.ram.read_byte(self.pc_reg);
         self.pc_reg += 1;
         if address > 0x80 {
             address + self.pc_reg as u8
@@ -195,47 +200,46 @@ impl CPU {
         }
     }
 
-    pub fn fetch_absolute(&mut self, nes: &mut NES) -> u16 {
-        let address = nes.wram.read_word(self.pc_reg);
+    pub fn fetch_absolute(&mut self) -> u16 {
+        let address = self.ram.read_word(self.pc_reg);
         self.pc_reg += 2;
         address & 0xffff
     }
 
-    pub fn fetch_absolute_x(&mut self, nes: &mut NES) -> u16 {
-        let address = nes.wram.read_word(self.pc_reg);
+    pub fn fetch_absolute_x(&mut self) -> u16 {
+        let address = self.ram.read_word(self.pc_reg);
         self.pc_reg += 2;
         (address + self.x_reg as u16) & 0xffff
     }
     
-    pub fn fetch_absolute_y(&mut self, nes: &mut NES) -> u16 {
-        let address = nes.wram.read_word(self.pc_reg);
+    pub fn fetch_absolute_y(&mut self) -> u16 {
+        let address = self.ram.read_word(self.pc_reg);
         self.pc_reg += 2;
         (address + self.y_reg as u16) & 0xffff
     }
 
-    pub fn fetch_indirect_x(&mut self, nes: &mut NES) -> u16 {
-        let address = nes.wram.read_byte(self.pc_reg);
+    pub fn fetch_indirect_x(&mut self) -> u16 {
+        let address = self.ram.read_byte(self.pc_reg);
         let tmp = ((address + self.x_reg) & 0xff) as u16;
-        let result = nes.wram.read_word(tmp);
+        let result = self.ram.read_word(tmp);
         self.pc_reg += 1;
 
-        (nes.wram.read_word(result) + nes.wram.read_word(result + 1) * 0x100)
+        (self.ram.read_word(result) + self.ram.read_word(result + 1) * 0x100)
     }
 
-    pub fn fetch_indirect_y(&mut self, nes: &mut NES) -> u16 {
-        let address = nes.wram.read_word(self.pc_reg);
-        let base = ((nes.wram.read_word(address) + nes.wram.read_word(address + 1)) & 0x00ff) * 0x100;
+    pub fn fetch_indirect_y(&mut self) -> u16 {
+        let address = self.ram.read_word(self.pc_reg);
+        let base = ((self.ram.read_word(address) + self.ram.read_word(address + 1)) & 0x00ff) * 0x100;
         self.pc_reg += 1;
 
         (base as u16 + self.y_reg as u16) & 0xffff
     }
 
-    pub fn execute_instruction(&self, nes: &mut NES, address: u16, opcode: &OpCode) {
+    pub fn execute_instruction(&mut self, address: u8, opcode: &OpCode) {
         match opcode.label {
-            Label::ADC => adc(nes, address as u8),
-            Label::LDA => lda(nes, address as u8),
+            Label::ADC => adc(self, address as u8),
+            Label::LDA => lda(self, address as u8),
             _ => println!("Other instructions")
         }
     }
 }
-
