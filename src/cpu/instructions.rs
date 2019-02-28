e51d316d3e9da750b5388b410e4ad5d97bcb9bec
evgendn <evgeniydein@gmail.com>
2019-02-28 15:30:24 +0500

    Refactor a little bit adc instruction

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -63,14 +63,11 @@ pub fn adc(cpu: &mut cpu::CPU, address: u8) {
     let memory = cpu.ram.read_byte(address as u16);
     let carry = cpu.p_reg.get_carry_flag() as u8;
     let accum = cpu.a_reg;
-    let result = accum as u16 + memory as u16 + carry as u16;
-    cpu.a_reg = result as u8;
+    cpu.a_reg = accum + memory + carry;
 
+    let result = accum as u16 + memory as u16 + carry as u16;
     cpu.p_reg.set_carry_flag(result > 0xff);
     cpu.p_reg.set_zero_flag(result == 0x0);
-
-    let acc = cpu.a_reg;
-    cpu.p_reg.set_overflow_flag((acc ^ memory) & (acc ^ result as u8) & 0x80 != 0);
-
+    cpu.p_reg.set_overflow_flag((accum ^ memory) & (accum ^ result as u8) & 0x80 != 0);
     cpu.p_reg.set_negative_flag(result & 0x80 == 0x80);
 }
